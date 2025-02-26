daa41aa6b63097d617c5defa9d5655b520a5fea7
UP240222 <UP240222@alumnos.upa.edu.mx>
2025-02-26 10:39:20 -0600

    dia 4

---
+++ Dia 4.py
@@ -0,0 +1,17 @@
+#1 Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'
+print ( 'Thirty' + ' days' + ' of' ' python')
+
+#2 Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+print ( 'Coding' + ' for' + ' all')
+
+#3 Declare a variable named company and assign it to an initial value "Coding For All".
+company = "Coding for all"
+
+#4  Print the variable company using _print()_.
+print ( company )
+
+#5 Print the length of the company string 
+len_company=len (company)
+print ( len_company)
+
+#6 Change all the characters to uppercase letters using _upper()_ method.
