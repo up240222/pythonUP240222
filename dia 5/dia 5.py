7c446e7860a3462d0859dc6b76d84ea50756a33f
UP240222 <UP240222@alumnos.upa.edu.mx>
2025-03-04 09:38:38 -0600

    day 5

---
+++ dia 5/dia 5.py
@@ -0,0 +1,17 @@
+#DAY 5
+#1 Declare an empty list
+empty_list =list ()
+
+#2Declare a list with more than 5 items
+print (cultures ['asian' , 'latino' , 'african' , 'indian' , 'russian'])
+
+#3Find the length of your list
+
+#4Get the first item, the middle item and the last item of the list
+
+#5Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+
+#6 Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+
+#7 Print the list using print()
+
