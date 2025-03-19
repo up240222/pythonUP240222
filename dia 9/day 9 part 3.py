2abf66efb844b317b66c7cfb267a3532fd24ad50
UP240222 <UP240222@alumnos.upa.edu.mx>
2025-03-19 10:20:52 -0600

    day 9 part 3

---
+++ dia 9/day 9 part 3.py
@@ -0,0 +1,52 @@
+## Exercises: Level 3
+
+#1. Here we have a person dictionary. Feel free to modify it!
+
+
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'}}
+print (person)
+mskills=len(person.get('skills'))//2
+print('the middle skills is', person['skills'][mskills])
+print('the word python is in person?' ,('Python' in person['skills']) )
+
+frontdeveloper='JavaScript' and 'React' in person['skills']
+backend_developer='Node', 'Python', 'MongoDB' in person['skills']
+fullstackdeveloper='React' and 'Node' and 'MongoDB' in person ['skills']
+unknown='unknown title'
+
+if frontdeveloper ==True and len(person['skills'])==2:
+    print('He is a front end developer')
+elif backend_developer ==True :
+    print('He is a backend developer')
+elif fullstackdeveloper ==True :
+    print('He is a fullstack developer')
+else:
+    print('unknown title')
+
+if person['is_married']== True and person['country']=='Finland':
+    print('Asabeneh Yetayeh lives in Finland. He is married')
+else:
+    print('it doesn´t match')
+'''
+    
+
+
+
+     * Check if the person dictionary has skills key, if so print out the middle skill in the skills list.
+     * Check if the person dictionary has skills key, if so check if the person has 'Python' skill and print out the result.
+     * If a person skills has only JavaScript and React, print('He is a front end developer'), if the person skills has Node, Python, MongoDB, print('He is a backend developer'), if the person skills has React, Node and MongoDB, Print('He is a fullstack developer'), else print('unknown title') - for more accurate results more conditions can be nested!
+     * If the person is married and if he lives in Finland, print the information in the following format:
+
+py
+    Asabeneh Yetayeh lives in Finland. He is married.
+'''
+
---
+++ dia 9/day 9.py
@@ -0,0 +1,51 @@
+### Exercises: Level 1
+
+#1. Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. Output:
+
+'''sh
+Enter your age: 30
+You are old enough to learn to drive.
+Output:
+Enter your age: 15
+You need 3 more years to learn to drive.
+'''
+age= int(input('put your age'))
+if age>=18: 
+    print('you are old enough to learn to drive')
+else:
+    dif= 18-age 
+    print('You need', dif , 'more years to learn to drive')
+
+#2. Compare the values of my_age and your_age using if … else. Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input. You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences, and a custom text if my_age = your_age. Output:
+
+    '''sh
+    Enter your age: 30
+    You are 5 years older than me.
+    '''
+mi_edad=(int(input('put your age')))
+tu_edad=(int(input('put the other age')))
+diff=int(tu_edad-mi_edad)
+if mi_edad == tu_edad:
+    print('they are the same age', mi_edad)
+
+elif diff==-1 or diff==1:
+    diff=1 
+    print('the difference is' , diff , 'year')
+else:
+    print('the difference is' , diff,  'years')
+
+#3. Get two numbers from the user using input prompt. If a is greater than b return a is greater than b, if a is less b return a is smaller than b, else a is equal to b. Output:
+
+'''sh
+Enter number one: 4
+Enter number two: 3
+4 is greater than 3
+'''
+a=int(input('put a number'))
+b=int(input('put another number'))
+if a>b:
+    print(a , 'is greater than', b)
+elif a<b:
+    print(a, 'is smaller tan', b)
+else:
+    print(a, 'is equal to', b)
