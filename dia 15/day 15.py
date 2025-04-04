f8489796ce2566d2921ccf3e0d8c38c82c33361a
UP240222 <UP240222@alumnos.upa.edu.mx>
2025-04-04 14:55:34 -0600

    day 15 completed

---
+++ dia 15/day 15.py
@@ -0,0 +1,23 @@
+#Open you python interactive shell and try all the examples covered in this section.
+#ERRORS
+#de sintaxis
+print('no cerrar el parentesis'
+#de nombre str 
+print(octavio no se baña)
+#index
+numeros=[1,2,3,4,5,]
+print(numeros[20])
+#modulo
+import nigger 
+#attribute
+import.range
+range.ends
+#key
+cosas={'pelotas', 'balones', 'bates','tachones','medias'}
+print(cosas[bat])
+#Type
+print('ss'+200)
+#valor
+proint float('ya que se acabe el cuatri')
+#import     
+from math import power 3
