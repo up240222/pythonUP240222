27190cc1488450bf65afaa96b963af0b00471805
UP240222 <UP240222@alumnos.upa.edu.mx>
2025-04-04 13:53:51 -0600

    day 14 part 2

---
+++ dia 14/day 14.py
@@ -0,0 +1,63 @@
+#LEVEL 1
+#1 Explain the difference between map, filter, and reduce
+
+#ej
+'''numeros = [1, 2, 3, 4]
+cuadrados = list(map(lambda x: x**2, numeros))
+print(cuadrados)  # [1, 4, 9, 16]'''# la funcion de lambda x:x**2 significa que al valor x o sea cada elemnto de la lista se le aplica lo que sigue de dos puntos  y maps es para aplicarlo en cada uno de los elementos y filter es para solo sacar a los que si cumplen la funcion
+'''
+map es el que aplica una función a cada elemento de una lista 
+filter es para solo sacar a los que si cumplen la funcion por eso el nombre de filtrar
+Reduce una lista a un solo valor aplicando una función acumulativa. Necesitas importarla desde functools.
+
+'''
+#2 Explain the difference between higher order function, closure and decorator
+'''
+Función de Orden Superior (Higher Order Function)
+Es una función que:
+-Recibe otra función como argumento, o
+
+-Devuelve una función como resultado. es decir, es una funcion donde le enseñas lo que quieres hacer
+ejemplo:
+def saludar(nombre):
+    return f"Hola, {nombre}"
+def procesar_saludo(funcion):
+    return funcion("Lucía")
+
+print(procesar_saludo(saludar)) aqui ya le enseñaste que es saludar y solo llamas la funcion con otra funcion
+
+Una closure es una función interna que recuerda las variables del entorno donde fue creada, incluso si ese entorno ya terminó.
+Ejemplo:
+
+python
+Copiar
+Editar
+def crear_multiplicador(factor):
+    def multiplicar(numero):
+        return numero * factor
+    return multiplicar
+
+por_dos = crear_multiplicador(2)
+print(por_dos(5))  
+
+Un decorador es una función que modifica o extiende el comportamiento de otra función, sin cambiar su código original.
+
+'''
+#3Define a call function before map, filter or reduce, see examples
+'Significa que antes de hacer el map, filter o reduce, tú defines primero la función con def, y luego la pasas como argumento, en lugar de usar lambda.'
+#4 Use for loop to print each country in the countries list.
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+for country in countries:
+    print(country)
+
+#5 Use for to print each name in the names list.
+names= ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway','Iceland']
+for name in names:
+    print(name)
+
+#6 Use for to print each number in the numbers list.
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for num in numbers:
+    print(num)
+
+
---
+++ dia 14/day 14part2.py
@@ -0,0 +1,107 @@
+#LEVEL2
+#1 Use map to create a new list by changing each country to uppercase in the countries list
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+def uppercase (country):
+    return country.upper()
+result=map(uppercase, countries)
+print(list(result))
+
+#2 Use map to create a new list by changing each number to its square in the numbers list
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def square_num (num):
+    return num**2
+result=map(square_num, numbers)
+print(list(result))
+
+#3 Use map to change each name to uppercase in the names list
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+def uppercase_name(name):
+    return name.upper()
+resul=map(uppercase_name, names)
+print(list(resul))
+
+#4 Use filter to filter out countries containing 'land'.
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+def lan_country (country):
+    if 'land' in country:
+        return True
+    return False
+result= filter(lan_country, countries)
+print(list(result))
+
+#5 Use filter to filter out countries having exactly six characters.
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+def six_ch_country(ch):
+    if len(ch)==6:
+        return True
+    return False
+result=filter(six_ch_country,countries )
+print(list(result))
+
+#6  Use filter to filter out countries containing six letters and more in the country list.
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+def six_ch_or_more_country(pais):
+    if len(pais)>=6:
+        return True
+    return False
+result=filter(six_ch_or_more_country, countries)
+print(list(result))
+
+#7 Use filter to filter out countries starting with an 'E'
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+def countries_w_e(pais):
+    return pais.startswith('E')
+result=(filter(countries_w_e,countries ))
+print(list(result))
+
+#8 Chain two or more list iterators (eg. arr.map(callback).filter(callback).reduce(callback))
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+from functools import reduce
+resultado=( lambda x, y: x + y,map(lambda x: x * 2, filter(lambda x: x % 2 == 0, numbers)))
+print(resultado)
+
+#9 Declare a function called get_string_lists which takes a list as a parameter and then returns a list containing only string items.
+lista = [1,2,3,'LETRAS','Paulo','palabras']
+def get_string_list(list):
+    return[cosa for cosa in lista if isinstance(cosa, str)]
+resultado=get_string_list(lista)
+print(resultado)
+
+#10 Use reduce to sum all the numbers in the numbers list.
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def add_num (x,y):
+    return int(x) + int(y)
+suma=reduce(add_num, numbers)
+print(suma)
+
+#11 Use reduce to concatenate all the countries and to produce this sentence: Estonia, Finland, Sweden, Denmark, Norway, and Iceland are north European countries
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+def pais_sentence(x,y):
+    return x + ', ' + y 
+oracion=reduce(pais_sentence,countries )
+oracion1= oracion+'los paises son de Europa '
+print(oracion1)
+
+#12 Declare a function called categorize_countries that returns a list of countries with some common pattern (you can find the countries list in this repository as countries.js(eg 'land', 'ia', 'island', 'stan')).
+from countries_lista import countries 
+
+def categorize_countries(countries , pattern):
+    return list(filter(lambda pais: pattern in pais.lower(),countries))
+print(categorize_countries(countries,'land'))
+
+#13 Create a function returning a dictionary, where keys stand for starting letters of countries and values are the number of country names starting with that letter.
+def count_countries_initial(countries ):
+    return dict(map(lambda letter: (letter, sum(1 for country in countries if country.startswith(letter))), sorted(set(map(lambda x: x[0], countries)))))
+print(count_countries_initial(countries))
+
+#14 Declare a get_first_ten_countries function - it returns a list of first ten countries from the countries.js list in the data folder.
+def get_first_ten():
+    ten= [p for p in countries[:10]]
+    return ten
+print(get_first_ten())
+
+#15 Declare a get_last_ten_countries function that returns the last ten countries in the countries list.
+def get_last_ten():
+    ten= [p for p in countries[-10:]]
+    return ten
+print(get_last_ten())
